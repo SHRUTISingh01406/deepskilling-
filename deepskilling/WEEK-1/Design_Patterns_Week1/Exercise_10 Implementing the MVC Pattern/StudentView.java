91335c7ffbfb47d95f89082640f10665ea533894
Shruti Singh <821shrutisingh@gmail.com>
2026-07-09 14:35:57 +0530

    week 1-dsa

--- deepskilling/WEEK-1/Design_Patterns_Week1/Exercise_10 Implementing the MVC Pattern/StudentView.java
+++
@@ -1,8 +0,0 @@
-public class StudentView {
-    public void displayStudentDetails(String studentName, int studentId, String studentGrade) {
-        System.out.println("Student Details:");
-        System.out.println("Name of Student: " + studentName);
-        System.out.println("Student ID: " + studentId);
-        System.out.println("Grade: " + studentGrade);
-    }
-}
